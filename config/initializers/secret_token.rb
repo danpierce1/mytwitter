1d01317cd546deeb3389318fd40c44cf1eb66fdf
Dan Pierce <20045260@mail.wit.ie>
2013-10-16 10:31:51 +0100

    adding files to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytwitter::Application.config.secret_token = '8a99ef246213d22a5d15c0f2db282bb1e260c28ec639853b614a49c6394d2bc7b76137d63f11722a8560145c93cac45e123570878a896d9289fc7af8e626450e'
